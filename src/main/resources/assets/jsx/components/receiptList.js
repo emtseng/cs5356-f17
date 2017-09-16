032d545c9b8cd01a1e9d9074d05047e9c5d3519c
emtseng <etseng42@gmail.com>
2017-09-16 12:11:14 -0400

    hooks up receipts to view

--- src/main/resources/assets/jsx/components/receiptList.js
+++ src/main/resources/assets/jsx/components/receiptList.js
@@ -1,5 +1,21 @@
-/* global React */
+import React from 'react'
 
 class ReceiptList extends React.Component {
-
+  render() {
+    return (
+      <div id="receiptList">
+        {
+          this.props.receipts && this.props.receipts.map(receipt => (
+            <div className="receipt">
+              <div className="merchant">{receipt.merchantName}</div>
+              <div className="amount">{receipt.value}</div>
+              <div className="tags">{receipt.tags}</div>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
 }
+
+export default ReceiptList
--- src/main/resources/assets/jsx/index.js
+++ src/main/resources/assets/jsx/index.js
@@ -1,10 +1,25 @@
-/* global React ReactDOM */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+import ReceiptList from './components/receiptList'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      receipts: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`/api/receipts`)
+    .then(res => this.setState({receipts: res.data}))
+  }
   render() {
     return (
       <div id="main">
         My React works!
+        <ReceiptList receipts={this.state.receipts} />
       </div>
     )
   }
