12db60515b21803a9e8d190cbcc27e4143e3dd3d
emtseng <etseng42@gmail.com>
2017-10-05 23:36:32 -0400

    last touches

--- src/main/resources/assets/jsx/components/SnapReceipt.js
+++ src/main/resources/assets/jsx/components/SnapReceipt.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import $ from 'jquery'
 
 class SnapReceipt extends Component {
   constructor(props) {
--- src/main/resources/assets/public/bundle.js
+++ src/main/resources/assets/public/bundle.js
@@ -34929,10 +34929,6 @@ var _react = __webpack_require__(15);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _jquery = __webpack_require__(212);
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
